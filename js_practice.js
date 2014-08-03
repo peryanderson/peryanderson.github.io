bd5dee84f85efffbfe1ee42d8f96cc66ef9fb262
Perry Anderson <peryanderson@gmail.com>
2014-08-03 12:34:06 -0500

    Adding blog posts

---
+++ js_practice.js
@@ -0,0 +1,4 @@
+var valueOne = 5;
+var valueTwo = 10;
+var result = valueOne + valueTwo;
+print result
---
+++ unit1_projects/gc1_pseudocode.js
@@ -0,0 +1,18 @@
+//taking array of numbers as input
+//start with empty variable sum
+//method with a for loop that adds each number in array to sum 
+//return sum 
+
+//taking array of numbers as input
+//start with empty variable sum
+//method with a for loop that adds each number in array to sum
+//create new varaible avrg that is equal to sum divded by array length
+//return avrg
+
+//taking array of numbers as input
+//method to sort the array from smallest to largest value
+//define variable equal to half array length called mid (keep in whole numbers)
+//if array length is odd
+//return member of array at mid position
+//if array length is even
+//return member of array at mid added to member of array at mid -1 divided by two (not whole numbers)
